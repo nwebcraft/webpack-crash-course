e0838d3fc24ce97ea8886f58ad60c19158280c67
nwebcraft_macbookpro13 <nwebcraft@nwebcraookpro13.AirPort>
2021-04-02 23:33:18 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import _ from 'lodash'
+import { NAME, NiJou } from './utilities'
+import * as utilities from './utilities'
+import Tiger from './utilities'
+
+console.log(NiJou(13))
+console.log(NAME)
+console.log(utilities.NAME)
+console.log(utilities.NiJou(12))
+console.log(Tiger.say())
 
 function component() {
   const element = document.createElement('div');
