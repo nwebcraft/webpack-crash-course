9e1be2493710341186e8d393ae87efcf356e0884
nwebcraft_macbookpro13 <nwebcraft@nwebcraookpro13.AirPort>
2021-04-03 00:49:32 +0900

    implement url-loader and rile-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import _ from 'lodash'
 import './style.css'
+import logo from './logo.png'
 
 function component() {
-  const element = document.createElement('div');
-  const array = ['Hello', 'webpack', '!!'];
-  element.innerHTML = _.join(array, ' ');
+  const element = document.createElement('div')
+  const array = ['Hello', 'webpack', '!!']
+  element.innerHTML = _.join(array, ' ')
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
 document.body.classList.add('haikei')
+
+const image = new Image()
+image.src = logo
+document.body.appendChild(image)
 
