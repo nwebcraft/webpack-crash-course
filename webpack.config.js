5b46eb43cae2cd897f404bab7b89ce39a633879d
nwebcraft_macbookpro13 <nwebcraft@nwebcraookpro13.AirPort>
2021-04-09 00:00:31 +0900

    apply uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -56,6 +57,17 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css'
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
+  }
 }
 
